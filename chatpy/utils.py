b882acb6db8f5373a6d4dda7e27a71ccbe9a410a
aqn <aqn000@gmail.com>
2013-12-07 05:10:19 +0900

    fix Syntax Error on 3.3

--- chatpy/utils.py
+++ chatpy/utils.py
@@ -89,7 +89,7 @@ def import_simplejson():
             try:
                 from django.utils import simplejson as json  # Google App Engine
             except ImportError:
-                raise ImportError, "Can't load a json library"
+                raise ImportError("Can't load a json library")
 
     return json
 
